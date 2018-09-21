f148b26516ec0fd1c6110b7357a7ca5bd9968b63
FcazBR <cbeferr@hotmail.com>
2018-09-20 22:38:21 -0300

    Adicionando projeto java

---
+++ AprendendoLinguagemJava/src/TiposDeDados.java
@@ -0,0 +1,19 @@
+
+public class TiposDeDados {
+
+    public static void main(String[] args) {
+        //textos
+        char letra = 'A';
+        String texto = "texto";
+
+        //lógico
+        boolean valor = true;
+        
+        //numérico
+        
+        int inteiro = 10;
+        long inteiroDobrado = 20;
+        float real = 10.0f;
+        double realDobrado = 20.0;
+    }
+}
